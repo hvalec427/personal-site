60378d579d669ba43db02eb83541170043cd4f99
Žiga Hvalec <zigahvalec@gmail.com>
2020-03-15 13:59:50 +0100

    fixed no language on first render

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,5 +2,6 @@ export const getLanFromUrl = () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const lan = urlParams.get('lan');
+    if (!lan) return 'en';
     return lan;
 };
