1f7a8741999a639ea2810fbf62750d7c630bb941
Žiga Hvalec <zigahvalec@gmail.com>
2025-08-17 10:14:12 +0200

    version 2 of personal website

--- src/js/utils.js
+++
@@ -1,7 +0,0 @@
-export const getLanFromUrl = () => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const lan = urlParams.get('lan');
-    if (!lan) return 'en';
-    return lan;
-};
